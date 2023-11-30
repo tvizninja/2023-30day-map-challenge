e17804bcf6efdfeb6c906ad25a2efc76ec344e4f
tvizninja <github@tviz.ninja>
2023-11-30 15:49:54 +0000

    add 30-myfav

---
+++ 30-myfav/app.js
@@ -0,0 +1,127 @@
+// node ./node_modules/browserify/bin/cmd.js -o viz/app.js
+const geojsonvt = require('geojson-vt')
+const vtpbf = require('vt-pbf')
+
+app = {}
+
+// image loader
+function loader(items, thingToDo, allDone) {
+  if (!items) { return }
+  if ("undefined" === items.length) { items = [items] }
+  let count = items.length
+  let thingToDoCompleted = function (items, i) {
+    count--
+    if (0 == count) { allDone(items) }
+  }
+  for (let i = 0; i < items.length; i++) {
+    thingToDo(items, i, thingToDoCompleted)
+  }
+}
+
+// add protocol
+maplibregl.addProtocol('custom', (params, callback) => {
+  const [z, x, y] = params.url.split('/').slice(2, 5).map(a=>parseInt(a.replace(/[^0-9]/g, ''), 10))
+  const imgs = [
+    `https://tiles.gsj.jp/tiles/elev/gsidemgm/${z}/${y}/${x}.png`,
+    `https://tiles.gsj.jp/tiles/elev/gsidemgm/${z}/${y+1}/${x}.png`
+  ]
+  const imgd = []
+  function loadImage(items, i, onComplete) {
+    const onLoad = e => {
+      const size = 256
+      e.target.removeEventListener("load", onLoad)
+      e.target.removeEventListener("error", onLoad)
+      if(e.type=='load'){
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size
+        const ctx = canvas.getContext('2d')
+        ctx.drawImage(img, 0, 0)
+        imgd[i] = ctx.getImageData(0, 0, canvas.width, canvas.height).data
+      }else{
+        imgd[i] = new Uint8Array(size * size * 4)
+      }
+      onComplete(items, i)
+    }
+    let img = new Image()
+    img.crossOrigin = "Anonymous"
+    img.addEventListener("load", onLoad, false)
+    img.addEventListener("error", onLoad, false)
+    img.src = items[i]
+  }
+  loader(imgs, loadImage, ()=>{
+    const pc = new Uint8Array(imgd[0].length + imgd[1].length)
+    pc.set(imgd[0])
+    pc.set(imgd[1], imgd[0].length)
+    const mlim = Math.asin(Math.tanh(Math.PI))/Math.PI * 180
+    const fn  = yy => 2*Math.atan(Math.tanh(Math.PI*yy/360))*180/Math.PI
+    const ipz = 1/Math.pow(2, z)
+    const coe = ipz*2*180
+    const lat = coe*-y + 180
+    const lon = coe*x  - 180
+    const r = 4
+    const n = 256/r
+    const h = ha => (Math.pow(0.002*z/r,2))*((128<=ha[0])?0:(256*256*ha[0] + 256*ha[1] + ha[2]))
+    const gj = {
+      "type": "FeatureCollection",
+      "features": [...Array(n+Math.floor(n/4))].map((a, i)=>{
+        return {
+          "type": "Feature",
+          "properties": {'color': `rgb(128, 128, ${Math.abs((lat+90)*255/180-i*r/Math.pow(2,z-1)-128)+128})`},
+          "geometry": {
+            "coordinates": 
+            [...Array(r*n)].map((b, j)=>[
+              lon+(j+j/(r*n))*coe/(r*n),
+              fn(lat-(i-h(pc.slice(4*(j+256*i*r), 4*(j+256*i*r)+3)))*coe/n)
+            ]),
+            "type": "LineString"
+          }
+        }
+      })
+    }
+    const tileindex = geojsonvt(gj, {maxZoom: z, indexMaxZoom: z})
+    const tile = tileindex.getTile(z, x, y)
+    if(tile){ callback(null, vtpbf.fromGeojsonVt({ 'lines': tile }), null, null) }
+
+  })
+  return { cancel: () => {} }
+})
+
+// map init
+app.map = new maplibregl.Map({
+  container: 'map',
+  style: {
+    "version": 8,
+    "sources": {
+      "gsi": {
+        "type": "vector",
+        "tiles": [ "custom://{z}/{x}/{y}.pbf" ],
+        "minzoom": 0,
+        "maxzoom": 8,
+        "attribution": "<a href=\"https://gbank.gsj.jp/seamless/elev/\" target=\"_blank\">DEM by 産業技術総合研究所</a>"
+      }
+    },
+    "layers": [
+      {
+        "id": "lines",
+        "type": "line",
+        "source": "gsi",
+        "source-layer": "lines",
+        "minzoom": 0,
+        "maxzoom": 8,
+        "paint": {
+          "line-color": ['get', 'color'],
+          "line-width": 2
+        }
+      }
+    ]
+  },
+  center: [135, 35],
+  maxPitch: 0,
+  minZoom: 3,
+  maxZoom: 7.9,
+  zoom: 3
+})
+app.map.on('load', ()=>{
+  app.map.addControl(new maplibregl.NavigationControl('top-right'))
+})
